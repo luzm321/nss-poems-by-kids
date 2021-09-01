6d2f6d63ae6b372d49d80b8991d94c64ced617ae
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-01 16:54:07 -0500

    initial commit for Poems By Kids (PoKi) SQL exercise

---
+++ PoKiDBQuery.sql
@@ -0,0 +1,54 @@
+--Investigating the Data
+--Query the PoKi database using SQL SELECT statements to answer the following questions:
+
+--What grades are stored in the database?
+SELECT 
+	Id, 
+	Name
+FROM Grade;
+
+--What emotions may be associated with a poem?
+SELECT 
+	Id,
+	Name [Emotion Name]
+FROM Emotion;
+
+--How many poems are in the database?
+SELECT
+	COUNT(Id) [Total Poem Count]
+FROM Poem;
+
+--Sort authors alphabetically by name. What are the names of the top 76 authors?
+
+
+--Starting with the above query, add the grade of each of the authors.
+
+--Starting with the above query, add the recorded gender of each of the authors.
+
+--What is the total number of words in all poems in the database?
+
+--Which poem has the fewest characters?
+
+--How many authors are in the third grade?
+
+--How many total authors are in the first through third grades?
+
+--What is the total number of poems written by fourth graders?
+
+--How many poems are there per grade?
+
+--How many authors are in each grade? (Order your results by grade starting with 1st Grade)
+
+--What is the title of the poem that has the most words?
+
+--Which author(s) have the most poems? (Remember authors can have the same name.)
+
+--How many poems have an emotion of sadness?
+
+--How many poems are not associated with any emotion?
+
+--Which emotion is associated with the least number of poems?
+
+--Which grade has the largest number of poems with an emotion of joy?
+
+--Which gender has the least number of poems with an emotion of fear?
